f79f9c1434f5634c4cd0c48cb13d5558bfd4805e
Michael DiSanto <mdisanto928@gmail.com>
2026-08-08 13:04:19 -0400

    updated to pull income statement and cash flow statement into financial ETL

--- src/financial_etl.py
+++ src/financial_etl.py
@@ -5,32 +5,55 @@ from config import FMP_API_KEY
 
 ticker = "MNST"
 
-url = (
+income_statement_url = (
     f"https://financialmodelingprep.com/stable/income-statement"
     f"?symbol={ticker}"
     f"&apikey={FMP_API_KEY}"
 )
 
-response = requests.get(url, timeout=30)
-response.raise_for_status()
+cash_flow_url = (
+    f"https://financialmodelingprep.com/stable/cash-flow-statement"
+    f"?symbol={ticker}"
+    f"&apikey={FMP_API_KEY}"
+)
 
-data = response.json()
+income_statement_response = requests.get(income_statement_url, timeout=30)
+income_statement_response.raise_for_status()
 
-df = pd.DataFrame(data)
+cash_flow_response = requests.get(cash_flow_url, timeout=30)
+cash_flow_response.raise_for_status()
 
-financials_df = df[
+income_statement_data = income_statement_response.json()
+cash_flow_data = cash_flow_response.json()
+
+income_statement_df = pd.DataFrame(income_statement_data)
+cash_flow_df = pd.DataFrame(cash_flow_data)
+
+
+
+income_df = income_statement_df[
     ["fiscalYear", "revenue", "netIncome", "eps"]
 ].copy()
+
+cash_df = cash_flow_df[
+    ["fiscalYear", "freeCashFlow"]
+].copy()
+ 
+financials_df = income_df.merge(
+    cash_df,
+    on="fiscalYear",
+    how="left"
+)
 
 financials_df = financials_df.rename(
     columns={
         "fiscalYear": "fiscal_year",
-        "netIncome": "net_income"
+        "netIncome": "net_income",
+        "freeCashFlow": "free_cash_flow"
     }
 )
 
 financials_df["company_id"] = 1
-financials_df["free_cash_flow"] = None
 
 financials_df = financials_df[
     [
