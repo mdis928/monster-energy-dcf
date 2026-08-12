fea72e75ca74482423ae1ea9586ff30f4fccc096
Michael DiSanto <mdisanto928@gmail.com>
2026-08-12 10:00:33 -0400

    Added Google Trends interest extraction. Ran into 429 Too Man Requests

--- src/trends_etl.py
+++ src/trends_etl.py
@@ -8,3 +8,7 @@ pytrends = TrendReq(hl='en-US', tz=360)
 kw_list = ["Monster Energy"] 
 
 pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+
+trends_etl = pytrends.interest_over_time()
+
+print(trends_etl)
